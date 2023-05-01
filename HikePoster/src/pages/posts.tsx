37336af495e9a33679be653349d694e96abe8d49
Eric Wu <eric.wu151@gmail.com>
2023-05-01 17:53:27 -0600

    Loading posts from firebase

--- HikePoster/src/pages/posts.tsx
+++ HikePoster/src/pages/posts.tsx
@@ -21,6 +21,16 @@ export const Posts = () => {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
+    const watch = navigator.geolocation.watchPosition((location) => {
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      setLocationLoaded(true);
+    }, (err) => {
+      console.log(err)
+    }, {
+      enableHighAccuracy: true,
+    })
+
     // TODO load users rooms
     async function loadPosts() {
       // get posts from db
@@ -31,57 +41,28 @@ export const Posts = () => {
       setPosts([...posts]);
     }
     loadPosts();
+
+    return () => navigator.geolocation.clearWatch(watch);
+
   }, []);
 
 
-  function makeTestPosts() {
-    var posts = []
-    for (let i = 0; i < 10; i++) {
-      var post = []
-      post[0] = "/src/assets/test.jpg"
-      post[1] = "London Bridge" + i
-      post[2] = "description" + i
-      post[3] = getCurrentGPSLocation()
-      posts[i] = post
-    }
-    return posts
-  }
+  // function makeTestPosts() {
+  //   var posts = []
+  //   for (let i = 0; i < 10; i++) {
+  //     var post = []
+  //     post[0] = "/src/assets/test.jpg"
+  //     post[1] = "London Bridge" + i
+  //     post[2] = "description" + i
+  //     post[3] = getCurrentGPSLocation()
+  //     posts[i] = post
+  //   }
+  //   return posts
+  // }
 
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
   const [locationLoaded, setLocationLoaded] = useState(false);
-  function getCurrentGPSLocation() {
-    useEffect(() => {
-      // navigator.geolocation.getCurrentPosition((location) => {
-      //   setLocationLoaded(true);
-      //   setLat(location.coords.latitude);
-      //   setLon(location.coords.longitude);
-      // }, (err) => {
-      //   console.log(err)
-      // }, {
-      //   enableHighAccuracy: true,
-      // })
-      const watch = navigator.geolocation.watchPosition((location) => {
-        setLat(location.coords.latitude);
-        setLon(location.coords.longitude);
-        setLocationLoaded(true);
-      }, (err) => {
-        console.log(err)
-      }, {
-        enableHighAccuracy: true,
-      })
-
-      return () => navigator.geolocation.clearWatch(watch)
-    }, []);
-
-    return (
-      <div className="location">
-        {locationLoaded ? (`Lat: ${lat}, Lon: ${lon}`) : 'Loading...'}
-      </div>
-    )
-  }
-
-
 
   function saveToDB(image: any, title: string, description: string, location: string) {
     // save to db
@@ -95,26 +76,21 @@ export const Posts = () => {
     //const x = 10
     // for each post in db, return photograph attached, title, description, and location
     const returnMe = posts.map((post) => (
-      <>
-        <div key={post.id} className={"post"}>
-              
-          <h2>{ post.title }</h2>
-          {/* isnert random image */}
-          {/* https://picsum.photos/200/300 */}
-          <img src={ post.image } alt="viewpoint" />
-          { /* take the image and run it through findGPS */}
-          <p>description { post.description }</p>
-          <div className="location">
-            location
-            <p>Lat: { post.lat }, Lon: { post.lon }</p>
-            <p>ID: { post.id }</p>
-          </div>
+      <div key={post.id} className={"post"}>
+            
+        <h2>{ post.title }</h2>
+        {/* isnert random image */}
+        {/* https://picsum.photos/200/300 */}
+        <img src={ post.image } alt="viewpoint" />
+        { /* take the image and run it through findGPS */}
+        <p>description { post.description }</p>
+        <div className="location">
+          location
+          <p>Lat: { post.lat }, Lon: { post.lon }</p>
+          <p>ID: { post.id }</p>
         </div>
-        <br></br>
-        <br></br>
-      </>
+      </div>
     ))
-    console.log(returnMe);
 
     return (
       <>
@@ -124,28 +100,37 @@ export const Posts = () => {
 
   }
 
-  async function uploadFile(e: React.ChangeEvent<HTMLInputElement>) {
-    const target = e.target as HTMLInputElement;
-    const file = target.files?.[0];
-    const fileRef = ref(storage, `/images/${file!.name || ''}`)
-    const snapshot = await uploadBytes(fileRef, file!);
+  async function submitPost() {
+
+    if (imageFile == null || title == "" || description == "" || user == null) {
+      return;
+    }
+
+    const fileRef = ref(storage, `/images/${imageFile.name || ''}`)
+    const snapshot = await uploadBytes(fileRef, imageFile);
 
     // likely in different place
-    const downloadUrl = await getDownloadURL(ref(storage, `/images/${file!.name || ''}`));
+    const downloadUrl = await getDownloadURL(ref(storage, `images/${imageFile.name}`));
 
-    // Add a new document with a generated id.
-    const docRef = await addDoc(collection(db, "posts"), {
+    const postData = {
       image: downloadUrl,
       title: title,
       description: description,
-      lat: lat,
-      lon: lon,
-      author: user?.uid
-    });
+      lat: lat.toString(),
+      lon: lon.toString(),
+      author: user?.uid || ''
+    }
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "posts"), postData);
+
+    posts.push({...postData, id: docRef.id} as Post);
+    setPosts([...posts]);
+    
     console.log("Document written with ID: ", docRef.id);
 
   }
 
+  const [imageFile, setImageFile] = useState<File>();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
@@ -153,13 +138,14 @@ export const Posts = () => {
     <>
       <div className="header">
         <h1>HikePoster</h1>
+        {locationLoaded ? (`Lat: ${lat}, Lon: ${lon}`) : 'Loading...'}
         <div>Make a Post</div>
         {/* File browser for selection */}
         <textarea value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title"></textarea>
         <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description"></textarea>
-        <input type="file" onChange={uploadFile}/>
+        <input type="file" onChange={(e) => setImageFile(e.target.files?.[0])}/>
         <br></br>
-        <button>Submit Post</button>
+        <button onClick={() => {submitPost()}}>Submit Post</button>
         <button onClick={() => signOut(auth)}>Logout</button>
         <br></br>
         <br></br>
