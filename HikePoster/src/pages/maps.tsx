7e7e1a7e345adf6d7ee6a1bda66d007b60a12df4
Jensen Judkins <thesmelliestman@gmail.com>
2023-05-02 04:18:31 -0600

    added google.maps api+key for data tracking

--- HikePoster/src/pages/maps.tsx
+++ HikePoster/src/pages/maps.tsx
@@ -5,13 +5,18 @@
 
 var api_key = "AIzaSyAc9zFpuwRk68scBCEq2Y1B1yumE19sgGE";
 
-var lat = 40.714224;
-var lng = -73.961452;
+// var lat = 40.714224;
+// var lng = -73.961452;
 
-var request = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=" + api_key;
 
-function Maps(latitude: number, longitude: number) {
 
+async function Maps(latitude: number, longitude: number) {
+
+    var request = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=" + api_key;
+    var apiTracking = await fetch(request)
+    var apiTrackingJson = await apiTracking.json()
+    var address = apiTrackingJson.results[0].formatted_address
+    console.log(address)
     open("https://www.google.com/maps/search/?api=1&query=" + latitude + "%2C" + longitude)
     //open("https://maps.google.com/?q="+ latitude + "," + longitude)
 
